8f5a77b84c111ac0c7f5e0f8720c870510839d4c
Thorsten Sick <thorsten.sick@primion.eu>
2023-07-17 09:22:11 +0200

    Adding sphinx documentation for command line

--- checkbox_empire.py
+++ checkbox_empire.py
@@ -42,35 +42,39 @@ def mark_control(largs: argparse.Namespace) -> None:
 
     cbe.save_toml_database()
 
-
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(description='Manage compliance documents')
-    parser.add_argument('--config', type=str, default="config.toml", help='an integer for the accumulator')
-    subparsers = parser.add_subparsers(help='sub-commands')
+def create_parser():
+    """Create the command line parser."""
+    lparser = argparse.ArgumentParser(description='Manage compliance documents')
+    lparser.add_argument('--config', type=str, default="config.toml", help='The main configuration file')
+    subparsers = lparser.add_subparsers(help='sub-commands')
 
     # create the parser for the "export" command
     parser_export = subparsers.add_parser('export', help='Export generated data')
     parser_export.set_defaults(func=export)
-    parser_export.add_argument('--toml', action="store_true", default=False, help='export to toml')
-    parser_export.add_argument('--toml_file', default="testfile.toml", help='file name of the toml file to write')
+    parser_export.add_argument('--toml', action="store_true", default=False, help='Export to toml')
+    parser_export.add_argument('--toml_file', default="testfile.toml", help='File name of the toml file to write')
 
     # create the parser for the "show" command
-    parser_show = subparsers.add_parser('show', help='show control details')
+    parser_show = subparsers.add_parser('show', help='Show control details')
     parser_show.set_defaults(func=show)
-    parser_show.add_argument('uid', default=None, help='uid of the element to show details for')
+    parser_show.add_argument('uid', default=None, help='UID of the element to show details for')
 
     # create the parser for the "list" command
-    parser_list = subparsers.add_parser('list', help='list controls')
+    parser_list = subparsers.add_parser('list', help='List controls')
     parser_list.set_defaults(func=list_controls)
 
     # create the parser for the "mark" command
-    parser_mark = subparsers.add_parser('mark', help='mark a control with a state and a comment')
+    parser_mark = subparsers.add_parser('mark', help='Mark a control with a state and a comment')
     parser_mark.set_defaults(func=mark_control)
-    parser_mark.add_argument('uid', default=None, help='uid of the element to mark')
-    parser_mark.add_argument('state', default=None, help='state to write')
-    parser_mark.add_argument('--statement', default="", help='comment why this state is set')
+    parser_mark.add_argument('uid', default=None, help='UID of the element to mark')
+    parser_mark.add_argument('state', default=None, help='State to write')
+    parser_mark.add_argument('--statement', default="", help='Comment why this state is set')
 
+    return lparser
+
+if __name__ == "__main__":
+    parser = create_parser()
+    # TODO Add argcomplete
     args = parser.parse_args()
 
     args.func(args)
@@ -78,6 +82,5 @@ if __name__ == "__main__":
 
     # Parser commands:
     # export html
-    # mark item
 
     # Add OWASP WSTG https://github.com/OWASP/wstg/blob/master/checklists/checklist.json
---
+++ doc/source/index.rst
@@ -0,0 +1,25 @@
+.. Checkbox Empire documentation master file, created by
+   sphinx-quickstart on Mon Jul 17 08:32:02 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Checkbox Empire's documentation!
+===========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+   basics/background
+
+   usage/cli
+
+   basics/configuration
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
